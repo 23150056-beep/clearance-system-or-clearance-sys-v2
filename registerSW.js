9645a1ff7cf236daf0dd9d066f3353ce841c77da
23150056-beep <23150056-beep@users.noreply.github.com>
2026-01-31 17:29:03 +0000

    deploy: eff73cfe4f971d166c96368b9e28eb7549762188

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/clearance-system-or-clearance-sys-v2/sw.js', { scope: '/clearance-system-or-clearance-sys-v2/' })})}
